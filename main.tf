9366c8cad80908f3629090478dd6d2e60983b4b5
Lavakesh Pandey <Lavakesh.Pandey@maersk.com>
2021-02-25 12:32:04 +0530

    added scale set

--- main.tf
+++ main.tf
@@ -30,19 +30,19 @@ resource "azurerm_subnet" "web_server_subnet" {
   address_prefixes     = [each.value]
 }
 
-resource "azurerm_network_interface" "web_server_nic" {
-  name                = "${var.web_server_name}-${format("%02d", count.index)}-nic"
-  location            = var.web_server_location
-  resource_group_name = azurerm_resource_group.web_server_rg.name
-  count               = var.web_server_count
+# resource "azurerm_network_interface" "web_server_nic" {
+#   name                = "${var.web_server_name}-${format("%02d", count.index)}-nic"
+#   location            = var.web_server_location
+#   resource_group_name = azurerm_resource_group.web_server_rg.name
+#   count               = var.web_server_count
 
-  ip_configuration {
-    name                          = "${var.web_server_name}-ip"
-    subnet_id                     = azurerm_subnet.web_server_subnet["web-server"].id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = count.index == 0 ? azurerm_public_ip.web_server_public_ip.id : null
-  }
-}
+#   ip_configuration {
+#     name                          = "${var.web_server_name}-ip"
+#     subnet_id                     = azurerm_subnet.web_server_subnet["web-server"].id
+#     private_ip_address_allocation = "dynamic"
+#     public_ip_address_id          = count.index == 0 ? azurerm_public_ip.web_server_public_ip.id : null
+#   }
+# }
 
 resource "azurerm_public_ip" "web_server_public_ip" {
   name                = "${var.resource_prefix}-public-ip"
@@ -77,34 +77,57 @@ resource "azurerm_subnet_network_security_group_association" "web_server_sag" {
   subnet_id                 = azurerm_subnet.web_server_subnet["web-server"].id
 }
 
-resource "azurerm_windows_virtual_machine" "webserver" {
-  name                  = "${var.web_server_name}-${format("%02d", count.index)}"
+resource "azurerm_virtual_machine_scale_set" "web_server" {
+  name                  = "${var.resource_prefix}-scale-set"
   location              = var.web_server_location
   resource_group_name   = azurerm_resource_group.web_server_rg.name
-  network_interface_ids = [azurerm_network_interface.web_server_nic[count.index].id]
-  size                  = "Standard_B1s"
-  admin_username        = "webserver"
-  admin_password        = "Passw0rd1234"
-  availability_set_id   = azurerm_availability_set.web_server_availability_set.id
-  count                 = var.web_server_count
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  upgrade_policy_mode   = "manual"
+  
+  sku {
+    name = "Standard_B1s"
+    tier = "Standard"
+    capacity = var.web_server_count
   }
 
-  source_image_reference {
+  storage_profile_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServerSemiAnnual"
     sku       = "Datacenter-Core-1709-smalldisk"
     version   = "latest"
   }
+
+  storage_profile_os_disk {
+    name = ""
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name_prefix = var.web_server_name
+    admin_username        = "webserver"
+    admin_password        = "Passw0rd1234"
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+  network_profile {
+    name = "web_server_network_profile"
+    primary = true
+
+    ip_configuration {
+      name = var.web_server_name
+      primary = true
+      subnet_id = azurerm_subnet.web_server_subnet["web-server"].id
+    }
+  }
 }
 
-resource "azurerm_availability_set" "web_server_availability_set" {
-  name                        = "${var.resource_prefix}.availability-set"
-  location                    = var.web_server_location
-  resource_group_name         = azurerm_resource_group.web_server_rg.name
-  managed                     = true
-  platform_fault_domain_count = 2
-}
+# resource "azurerm_availability_set" "web_server_availability_set" {
+#   name                        = "${var.resource_prefix}.availability-set"
+#   location                    = var.web_server_location
+#   resource_group_name         = azurerm_resource_group.web_server_rg.name
+#   managed                     = true
+#   platform_fault_domain_count = 2
+# }
